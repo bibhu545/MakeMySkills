8578c0ab3447af7e02e20f13d2291c0f03351c18
bibhu545 <bibhuranjan.545@gmail.com>
2020-03-05 16:05:22 +0530

    Added cors and account controller to api

---
+++ MakeMySkills/MakeMySkills/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MakeMySkills.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public String Index()
+        {
+            return "Hello world";
+        }
+    }
+}
--- MakeMySkills/MakeMySkills/Global.asax.cs
+++ MakeMySkills/MakeMySkills/Global.asax.cs
@@ -19,5 +19,17 @@ namespace MakeMySkills
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            //HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
     }
 }

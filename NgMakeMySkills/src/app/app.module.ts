7349ebad2aaf87922cb56c436977d63bd4b9cf9d
bibhu545 <bibhuranjan.545@gmail.com>
2020-03-05 15:34:54 +0530

    Added bootstrap, material and Initial components to start

--- NgMakeMySkills/src/app/app.module.ts
+++ NgMakeMySkills/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Home/home.component';
+import { LoginComponent } from './Components/Account/login.component';
+import { SignupComponent } from './Components/Account/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

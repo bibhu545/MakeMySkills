5ca3ff4762ae51ca18a2850e6b8d68c21d041f19
Eakshitha <eakshithakoribilli@gmail.com>
2020-03-18 18:54:17 +0530

    Updated Online-Test: All Markers Added

--- NgMakeMySkills/src/app/Components/OnlineTest/online-test.component.ts
+++ NgMakeMySkills/src/app/Components/OnlineTest/online-test.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/Services/common.service';
 import { Router } from '@angular/router';
+import { timer } from 'rxjs';
+
+const MARKER = {
+  'answered': 1,
+  'not-answered': 2,
+  'marked': 3,
+  'marked-and-answered': 4,
+  'not-visited': 5
+}
 
 @Component({
   selector: 'app-online-test',
@@ -9,16 +18,17 @@ import { Router } from '@angular/router';
 })
 export class OnlineTestComponent implements OnInit {
   topicWiseQuestionsList = [
-    {TopicNumber: 1, Name: 'Reasoning Ability', TotalQn: 11, Questions: [{Qnumber: 0, Text: '', Summary: '', Visited: false},{},{},{},{},{},{},{},{},{},{}]},
-    {TopicNumber: 2, Name: 'Quantitative Ability', TotalQn: 12, Questions: [{Qnumber: 0, Text: '', Summary: '', Visited: false},{},{},{},{},{},{},{},{},{},{},{}]},
-    {TopicNumber: 3, Name: 'Verbal Ability', TotalQn: 20, Questions: [{Qnumber: 0, Text: '', Summary: '', Visited: false},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]}
+    {TopicNumber: 1, Name: 'Reasoning Ability', TotalQn: 11, Questions: [{Qnumber: 0, Text: '', Summary: '', NotAnswered: false, Answered: false, Marked: false, MarkedAndAnswered: false, SelectedOptionId: 0, Options: []},{},{},{},{},{},{},{},{},{},{}]},
+    {TopicNumber: 2, Name: 'Quantitative Ability', TotalQn: 12, Questions: [{Qnumber: 0, Text: '', Summary: '', NotAnswered: false, Answered: false, Marked: false, MarkedAndAnswered: false, SelectedOptionId: 0, Options: []},{},{},{},{},{},{},{},{},{},{},{}]},
+    {TopicNumber: 3, Name: 'Verbal Ability', TotalQn: 20, Questions: [{Qnumber: 0, Text: '', Summary: '', NotAnswered: false, Answered: false, Marked: false, MarkedAndAnswered: false, SelectedOptionId: 0, Options: []},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]}
   ];
   selectedQuestionNo = '';
   prevVisitedTopicNo = 0;
   prevVisitedQuestionNo = 0;
-  currentQuestionText = '';
-  currentQuestionNumber = 0;
+  currentQuestionNo = 0;
   currentTopicNo = 0;
+  reachedEndText = false;
+  seconds = 0;
 
   constructor(private commonService: CommonService, private route: Router) {
   }
@@ -31,15 +41,30 @@ export class OnlineTestComponent implements OnInit {
         question.Qnumber = ++count;
         question.Text = 'T' + topic.TopicNumber + 'Q' + count + ' Data Sufficiency:<br>What is the age of Principal?<br>(I) There are 14 students in a class.<br>(II) The average age of students and principal is 16 years.<br>(III) The average ageof 14 students is 3 less than the average of 14 students and Principal.';
         question.Summary = "The ratio of ages of mother and son is 5:2 and mother's age is 16.";
-        question.Visited = false;
+        question.NotAnswered = false;
+        question.Answered = false;
+        question.Marked = false;
+        question.MarkedAndAnswered = false;
+        question.SelectedOptionId = 0;
+        question.Options = [{OptionNo: 1, Text: ''}, {OptionNo: 2, Text: ''}, {OptionNo: 3, Text: ''}, {OptionNo: 4, Text: ''}];
+        question.Options[0].Text = 'Both (II) and (III)';
+        question.Options[1].Text = 'Both (I) and (III)';
+        question.Options[2].Text = 'Both (I) and (II)';
+        question.Options[3].Text = 'All (I), (II) and (III)';
       });
     });
+    console.log(this.topicWiseQuestionsList);
     this.selectedQuestionNo = 'section1-qnshort1';
     this.prevVisitedTopicNo = 1;
     this.prevVisitedQuestionNo = 1;
-    this.currentQuestionText = this.topicWiseQuestionsList[0].Questions[0].Text;
-    this.currentQuestionNumber = 1;
+    this.currentQuestionNo = 1;
     this.currentTopicNo = 1;
+    const numbers = timer(5000, 1000);
+    //numbers.subscribe(x => {console.log(x);this.seconds = x;});
+  }
+
+  getFromCharCode(index) {
+    return String.fromCharCode('A'.charCodeAt(0) + index);
   }
 
   scrollAndActivateCurrentSection(currentTopicNo) {
@@ -56,16 +81,114 @@ export class OnlineTestComponent implements OnInit {
     this.setSelectedQuestion(topicNo, qNo);
   }
 
+  setNextQuestion() {
+    if(this.currentQuestionNo === this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions.length){
+      if(this.currentTopicNo === this.topicWiseQuestionsList.length) {
+        // finish test
+        this.setSelectedQuestion(this.currentTopicNo ,this.currentQuestionNo);
+        this.reachedEndText = true;
+      }
+      else {
+        this.scrollAndActivateCurrentSection(this.currentTopicNo + 1);
+      }
+    }
+    else {
+      this.scrollAndActivateCurrentQuestion(this.currentTopicNo, this.currentQuestionNo + 1);
+    }
+  }
+
   setSelectedQuestion(topicNo, qNo) {
     if (this.prevVisitedTopicNo !== 0 && this.prevVisitedQuestionNo !== 0 && (this.prevVisitedTopicNo + this.prevVisitedQuestionNo !== topicNo + qNo)) {
-      this.topicWiseQuestionsList[this.prevVisitedTopicNo - 1].Questions[this.prevVisitedQuestionNo - 1].Visited = true;
+      let marker = this.getMarkerForQNo(this.prevVisitedTopicNo,this.prevVisitedQuestionNo);
+      if(marker === 1){
+        this.enableOnlyMarker(MARKER.answered);
+      }
+      else if(marker === 2){
+        this.enableOnlyMarker(MARKER["not-answered"]);
+      }
+      else if(marker === 3){
+        this.enableOnlyMarker(MARKER.marked);
+      }
+      else if(marker === 4){
+        this.enableOnlyMarker(MARKER["marked-and-answered"]);
+      }
     }
+    this.reachedEndText = false;
     this.selectedQuestionNo = 'section' + topicNo + '-qnshort' + qNo;
     this.prevVisitedTopicNo = topicNo;
     this.prevVisitedQuestionNo = qNo;
-    this.currentQuestionText = this.topicWiseQuestionsList[topicNo - 1].Questions[qNo - 1].Text;
-    this.currentQuestionNumber = qNo;
+    this.currentQuestionNo = qNo;
     this.currentTopicNo = topicNo;
+  }
+
+  setSelectedOption(optionNo) {
+    this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].SelectedOptionId = optionNo;
+  }
+
+  toggleSelectedOption(optionNo) {
+    if(this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].SelectedOptionId === 0){
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].SelectedOptionId = optionNo;
+    }
+    else{
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].SelectedOptionId = 0;
+    }
+  }
+
+  setMarkForReviewMarker() {
+    if(this.getMarkerForQNo(this.currentTopicNo, this.currentQuestionNo) == 1) {
+      this.enableOnlyMarker(MARKER["marked-and-answered"]);
+    }
+    else {
+      this.enableOnlyMarker(MARKER.marked);
+    }
+  }
+
+  confirmSubmittingTest() {
+    //to-do
+  }
+
+  enableOnlyMarker(marker) {
+    if (marker === 1) {
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Answered = true;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].NotAnswered = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Marked = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].MarkedAndAnswered = false;
+    }
+    else if (marker === 2) {
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Answered = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].NotAnswered = true;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Marked = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].MarkedAndAnswered = false;
+    }
+    else if( marker === 3) {
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Answered = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].NotAnswered = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Marked = true;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].MarkedAndAnswered = false;
+    }
+    else if (marker === 4) {
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Answered = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].NotAnswered = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].Marked = false;
+      this.topicWiseQuestionsList[this.currentTopicNo - 1].Questions[this.currentQuestionNo - 1].MarkedAndAnswered = true;
+    }
+  }
+
+  getMarkerForQNo(topicNo, qNo) {
+    if(this.topicWiseQuestionsList[topicNo - 1].Questions[qNo - 1].SelectedOptionId !== 0) {
+      if(this.topicWiseQuestionsList[topicNo - 1].Questions[qNo - 1].Marked || this.topicWiseQuestionsList[topicNo - 1].Questions[qNo - 1].MarkedAndAnswered){
+        return MARKER["marked-and-answered"];
+      }
+      else{
+        return MARKER.answered;
+      }
+    }
+    else if(this.topicWiseQuestionsList[topicNo - 1].Questions[qNo - 1].Marked){
+      return MARKER.marked;
+    }
+    else{
+      return MARKER["not-answered"];
+    }
   }
 
 }
